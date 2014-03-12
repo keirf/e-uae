fcf7f67f39b1abd143c27c237c817f3faf34501e
Keir <keir.xen@gmail.com>
2014-03-12 18:27:04 +0000

    Add <unlicense.org> 'license' to supercard_pro source file.

--- src/supercard_pro.c
+++ src/supercard_pro.c
@@ -1,3 +1,7 @@
+/*
+ * This file is free and unencumbered software released into the public domain.
+ * For more information, please refer to <http://unlicense.org/>
+ */
 
 #include "sysconfig.h"
 #include "sysdeps.h"
